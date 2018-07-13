b07d91667536b29ca88d5af5b167fcd4cb669fec
SeockChan Yoon <chaneyoon@gmail.com>
2018-07-13 22:32:53 +0900

    users 테이블 관련 API 추가

---
+++ api/challenge.js
@@ -0,0 +1,13 @@
+const Sequelize = require('sequelize');
+const API = require('../config.js');
+
+class UserAPI {
+    constructor() { this.API = API }
+    async get(no) { return await API.Users.findOne({where: {user_id: no}}); }
+    async getByEmail(email) { return await API.Users.findOne({where: {email: email}}); }
+    async getByNickname(nickname) { return await API.Users.findOne({where: {nickname: nickname}}); }
+    async login(email, password) { return await API.Users.findOne({where: {email: email, password: password}}); }
+    async getList() { return await API.Users.findAll(); }
+}
+
+module.exports = new UserAPI();
