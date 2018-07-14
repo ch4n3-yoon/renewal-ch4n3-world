81d0bfed8fdea2d2a99b7b876e727f9c51d5db37
SeockChan Yoon <chaneyoon@gmail.com>
2018-07-14 19:28:31 +0900

    nCTF/logs/sequelize.log 파일에 로깅하도록 변경 및 .gitignore 에 logs 추가

--- api/function.js
+++ api/function.js
@@ -4,7 +4,7 @@ const moment = require('moment');
 exports.log_sequelize = (msg) => {
     var currTime = moment(new Date()).format("YYYY-MM-DD HH:mm:ss")
     var text = `[${currTime}] ${msg}`
-    fs.appendFile('./logs/sequelize.log', text + '\n', 'utf8', (err) => {
+    fs.appendFile('../logs/sequelize.log', text + '\n', 'utf8', (err) => {
         if (err)
             console.log(err.message);
     });
