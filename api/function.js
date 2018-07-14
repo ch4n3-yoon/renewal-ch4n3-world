97a6e49c0604f8df39001fd4c209c2dafc27341c
SeockChan Yoon <chaneyoon@gmail.com>
2018-07-14 20:46:24 +0900

    isLogin() 함수 추가

--- api/function.js
+++ api/function.js
@@ -2,11 +2,26 @@ const fs = require('fs');
 const moment = require('moment');
 
 exports.log_sequelize = (msg) => {
-    var currTime = moment(new Date()).format("YYYY-MM-DD HH:mm:ss")
-    var text = `[${currTime}] ${msg}`
-    fs.appendFile('../logs/sequelize.log', text + '\n', 'utf8', (err) => {
+    let currTime = moment(new Date()).format("YYYY-MM-DD HH:mm:ss");
+    let text = `[${currTime}] ${msg}`;
+    if (!fs.existsSync('./logs')){
+        fs.mkdirSync('logs');
+    }
+    fs.appendFile('./logs/sequelize.log', text + '\n', 'utf8', (err) => {
         if (err)
             console.log(err.message);
     });
     //console.log(text)
+};
+
+exports.isLogin = (req, res) => {
+    if (!req.session.email) {
+        res.send(`<script>
+                    alert('Sorry, this page needs your login');
+                    history.back();
+                </script>`);
+        return 0;
+    }
+
+    return 1;
 };
