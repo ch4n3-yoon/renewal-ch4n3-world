dee88b4886fdf82bf1af307d5d789b2e5bbd1459
SeockChan Yoon <chaneyoon@gmail.com>
2018-07-14 20:45:59 +0900

    challenges.js에서 첫 sequelize 사용

--- api/challenge.js
+++ api/challenge.js
@@ -1,13 +1,13 @@
 const Sequelize = require('sequelize');
 const API = require('./config.js');
 
-class UserAPI {
+class ChallengeAPI {
     constructor() { this.API = API }
-    async get(no) { return await API.Users.findOne({where: {user_id: no}}); }
-    async getByEmail(email) { return await API.Users.findOne({where: {email: email}}); }
-    async getByNickname(nickname) { return await API.Users.findOne({where: {nickname: nickname}}); }
-    async login(email, password) { return await API.Users.findOne({where: {email: email, password: password}}); }
-    async getList() { return await API.Users.findAll(); }
+    async get(no) { return await API.Challenges.findOne({where: {no: no}}) }
+    async getByTitle(title) { return await API.Challenges.findOne({where: {title: title}}) }
+    async getByAuthor(author) { return await API.Challenges.findAll({where: {nickname: nickname}}) }
+    async getPointByNo(no) { return await API.Challenges.findOne({attributes: [no], where: {no: no}}) }
+    async getCategory() { return await API.Challenges.findAll({attributes: ['category'], where: {hidden: 0}, group: ['category']}) }
 }
 
-module.exports = new UserAPI();
+module.exports = new ChallengeAPI();
