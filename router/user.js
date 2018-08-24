daca25b6e2a0b01a679fddb1ed6a5c919b67504d
ch4n3-yoon <chaneyoon@gmail.com>
2018-08-24 09:49:12 +0900

    /user/{user_no} 유저 정보 출력 부분 코드 오류 수정 및 시퀄라이저 적용

--- router/user.js
+++ router/user.js
@@ -1,115 +1,70 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
-var lib = require('../lib.js');
-var conn = require('../dbconnect.js').conn;
+const API = require('../api/challenge');
+const userAPI = require('../api/user');
+const solversAPI = require('../api/solvers');
 
 router.get('/:no', async (req, res) => {
 
-    // 이메일이 주워지면, 해당 계정이 어떤 문제를 풀었는지 갖고 오는 코드
-    var getSolvedChall = async (email) => {
-
-        return new Promise(async (resolve, reject) => {
-            var query = "select solvedno, ";
-            query += "(select title from challenges where challenges.no = solvers.solvedno) as title, ";
-            query += "solvetime from solvers ";
-            query += "where email = ? order by solvetime desc";
-
-            var queryResult = await conn.query(query, [email], (err, rows) => {
-                resolve(rows);
-            });
-
-        });
-
+    let getTitle = async (chall_no) => {
+        let sqlData = await API.getByNo(chall_no);
+        if (sqlData)
+            return sqlData.dataValues.title;
+        return "undefined";
     };
 
-    // 해당 문제를 몇 번째로 풀었는지 갖고 오는 코드
-    var getSolveRank = async (solvedno, email) => {
-        return new Promise(async (resolve, reject) => {
-
-            var query = "select *, ";
-            query += "(select count(*) + 1 from solvers as s where s.solvetime < solvers.solvetime and s.solvedno = solvers.solvedno) as rank ";
-            query += "from solvers where solvedno = ? and email = ?";
-
-            var queryResult = conn.query(query, [solvedno, email], async (err, rows) => {
-                resolve(rows[0].rank);
-            });
-
-        });
+    let getSolveRank = async (chall_no, user_no) => {
+        let sqlData = await solversAPI.getSolvedTime(chall_no, user_no);
+        if (!sqlData)
+            return 0;
+        let solve_time = sqlData.dataValues.solve_time;
+        return await solversAPI.getSolvedRank(solve_time);
     };
 
-    var no2email = async (no) => {
-        return new Promise(async (resolve, reject) => {
-            var query = "select email from users where no = ?";
-            var queryResult = await conn.query(query, [no], async (err, rows) => {
-                resolve(rows[0].email);
-            });
-        });
-    }
+    let getSolvedChallenges = async (user_no) => {
+        let sqlData = await solversAPI.getSolvedChalls(user_no);
+        let challenges = [];
+        for (let i = 0; i < sqlData.length; i++)
+            challenges.push(sqlData[i].dataValues);
 
-    // hidden이 걸려 있지 않은 모든 문제들을 갖고 오는 코드
-    var getAllChallenges = async () => {
+        for (let i = 0; i < challenges.length; i++) {
+            let chall_no = challenges[i].challenge_no;
+            challenges[i].title = await getTitle(chall_no);
+            challenges[i].rank = await getSolveRank(chall_no, user_no);
+        }
 
-        return new Promise(async (resolve, reject) => {
-
-            var query = "select * from challenges where hidden = 0 order by point asc";
-
-            var queryResult = await conn.query(query, async (err, rows) => {
-                resolve(rows);
-            });
-
-        });
-
+        return challenges;
     };
 
-    // 이메일을 닉네임으로 변환시키는 코드
-    var no2nickname = async (no) => {
-        return new Promise(async (resolve, reject) => {
+    let getChallenegs = async () => {
+        let sqlData = await API.getChalls();
+        let challenges = [];
+        for (let i = 0; i < sqlData.length; i++) {
+            challenges.push(sqlData[i].dataValues);
+        }
 
-            var query = "select nickname from users where no = ? ";
-            var queryResult = await conn.query(query, [no], async (err, rows) => {
-                resolve(rows[0].nickname);
-            });
-
-        });
+        return challenges;
     };
 
+    let getNickname = async (user_no) => {
+        let sqlData = await userAPI.getNicknameByNo(user_no);
+        if (!sqlData)
+            return 0;
+        return sqlData.dataValues.nickname;
+    };
 
+    let main = async () => {
+        let user_no = req.params.no;
+        let solvedChallenges = await getSolvedChallenges(user_no);
+        let challenges = await getChallenegs();
+        let nickname = await getNickname(user_no);
 
-    var main = async () => {
+        console.log({solvedChallenges: solvedChallenges, allChallenges: challenges, nickname: nickname});
+        res.render('./user_info', {solvedChallenges: solvedChallenges, allChallenges: challenges, nickname: nickname});
+    };
 
-        return new Promise(async (resolve, reject) => {
-
-            let no = Number(req.params.no);
-            var email = await no2email(no);
-
-            var solvedChall = await getSolvedChall(email);
-            var solvedChallenges = [];
-
-            for (var i = 0; i < solvedChall.length; i++ ) {
-
-                // 해당 문제를 빠르게 푼 순위
-                var solvedRank = await getSolveRank(solvedChall[i].solvedno, email);
-
-                var solvedChallenge = {
-                    no: solvedChall[i].solvedno,
-                    title: solvedChall[i].title,
-                    solvetime: solvedChall[i].solvetime,
-                    rank: solvedRank
-                };
-
-                solvedChallenges.push(solvedChallenge);
-
-            }
-
-            res.render('./user_info', {solvedChallenges: solvedChallenges, allChallenges: await getAllChallenges(), nickname: await no2nickname(no)});
-
-        });
-
-    }
-
-    main();
-
+    await main();
 });
 
 
